4ffe4ebcf49bebd8ccc789c7c13e20903cb4dd93
Micky007 <michelecurcipio@gmail.com>
2018-11-28 00:45:00 +0100

    Switch to my favorite kernel toolchain
    Source of the tool = thttps://github.com/krasCGQ/aarch64-linux-android/tree/opt-linaro-7.x , thanks @KrasCGQ

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,8 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CONFIG := markw_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-opt-linux-android-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-opt-linaro-7.x/bin
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
