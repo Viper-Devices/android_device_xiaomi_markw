9c0d49af1f025a7768680a91aa5d216fd4dec943
Micky007 <michelecurcipio@gmail.com>
2018-12-09 23:47:53 +0100

    init.qcom.post_boots: increase default values for read ahead

--- rootdir/init.qcom.post_boot.sh
+++ rootdir/init.qcom.post_boot.sh
@@ -412,8 +412,8 @@ esac
 # Post-setup services
 case "$target" in
     "msm8937" | "msm8953")
-        echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
-        echo 512 > /sys/block/mmcblk0/queue/read_ahead_kb
+        echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
+        echo 1024 > /sys/block/mmcblk0/queue/read_ahead_kb
         setprop sys.post_boot.parsed 1
     ;;
 
